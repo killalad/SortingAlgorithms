e0a68a7e5575974b7b635a9e690c74c73c377c4c
Martin Georgiu <login@georgiu.cz>
2020-03-07 11:54:45 +0100

    first commit

---
+++ SortingAlgorithms/HeapSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class HeapSort
+    {
+        private int[] array;
+        public HeapSort(int[] rawArray) { array = rawArray; }
+
+        // sort the array
+        public int[] sort()
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                HeapMax(i);
+                swap(0, (array.Length - 1 - i));
+            }
+            return array;
+        }
+
+        //get the highest number in remaining unsorted part of array
+        private void HeapMax(int state)
+        {
+            int lowestParentPosition = ((array.Length - 1 - state) / 2);
+            for (int i = lowestParentPosition - 1; i >= 0; i--)
+            {
+                SortTriangle(i);
+            }
+        }
+
+        // swap values
+        private void swap(int first, int second)
+        {
+            int temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+
+        // sort the abstract heap triangle
+        private void SortTriangle(int parentPosition)
+        {
+            int firstChildPosition = 2 * parentPosition + 1;
+            int secondChildPosition = 2 * parentPosition + 2;
+
+            if (firstChildPosition < array.Length && array[parentPosition] < array[firstChildPosition])
+            {
+                swap(parentPosition, firstChildPosition);
+            }
+            if (secondChildPosition < array.Length && array[parentPosition] < array[secondChildPosition])
+            {
+                swap(parentPosition, secondChildPosition);
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter comma separated integers");
+            Console.WriteLine("eg.  5,9,12,2,3,6,7,10,11,1,8");
+            Console.WriteLine("");
+
+            // get data
+            String inputData = Console.ReadLine();
+
+            // parse data
+            inputData = inputData.Replace(" ", "");
+            String[] rawStringArray = inputData.Split(',');
+            int[] rawArray = Array.ConvertAll(rawStringArray, s => int.Parse(s));
+
+            // sort data
+            HeapSort heapSort = new HeapSort(rawArray);
+            int[] sortedArray = heapSort.sort();
+
+            // print data
+            Console.WriteLine("");
+            Console.WriteLine("Result:");
+            Console.WriteLine("[{0}]", string.Join(", ", sortedArray));
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to close this window . . .");
+            Console.ReadKey();
+        }
+    }
+}
