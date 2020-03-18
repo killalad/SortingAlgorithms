34c7771fb5f8e98e8f214b1103b8e1c64399d716
Martin Georgiu <login@georgiu.cz>
2020-03-18 16:05:43 +0100

    new HeapSortView

---
+++ SortingAlgorithms/HeapSortView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class HeapSortView
+    {
+        public static void intro()
+        {
+            Console.WriteLine("Enter comma separated integers");
+            Console.WriteLine("eg.  5,9,12,2,3,6,7788,7,10,11,1,8");
+            Console.WriteLine("");
+        }
+        public static void printArrayData(int[] array)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Result:");
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to close this window . . .");
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -6,16 +6,13 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter comma separated integers");
-            Console.WriteLine("eg.  5,9,12,2,3,6,7788,7,10,11,1,8");
-            Console.WriteLine("");
+            HeapSortView.intro();
 
             // get data
             String inputData = Console.ReadLine();
 
             // parse data
-            inputData = inputData.Replace(" ", "");
-            String[] rawStringArray = inputData.Split(',');
+            String[] rawStringArray = inputData.Replace(" ", "").Split(',');
             int[] rawArray = Array.ConvertAll(rawStringArray, s => int.Parse(s));
 
             // sort data
@@ -23,11 +20,8 @@ namespace SortingAlgorithms
             int[] sortedArray = heapSort.sort();
 
             // print data
-            Console.WriteLine("");
-            Console.WriteLine("Result:");
-            Console.WriteLine("[{0}]", string.Join(", ", sortedArray));
-            Console.WriteLine("");
-            Console.WriteLine("Press any key to close this window . . .");
+            HeapSortView.printArrayData(sortedArray);
+
             Console.ReadKey();
         }
     }
